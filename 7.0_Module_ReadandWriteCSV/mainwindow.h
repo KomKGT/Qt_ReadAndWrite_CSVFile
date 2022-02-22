c3407d980e39f5c797bef2add517c38e312a3a29
KomKGT <81642936+KomKGT@users.noreply.github.com>
2022-02-22 15:15:28 +0700

    Add files via upload

---
+++ 7.0_Module_ReadandWriteCSV/mainwindow.h
@@ -0,0 +1,60 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QMessageBox>
+#include <QVector>
+#include <QString>
+#include <QTextStream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <QFileSystemWatcher>
+#include <QMessageBox>
+#include <QCloseEvent>
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+public slots:
+
+    void openFile_dialog();
+    void saveFile_dialog();
+    void File_is_change();
+    void File_is_change_init();
+    void saveFile_MessageBox();
+    void closeEvent(QCloseEvent *event);
+
+
+
+
+
+
+
+private:
+    Ui::MainWindow *ui;
+
+    QString str_x[100000];
+    QString str_y[100000];
+    QVector<double> v_x;
+    QVector<double> v_y;
+
+    QVector<double> qv_x,qv_y;
+
+    void setcurrentFile(const QString &fileName);
+    QString currFile;
+
+    QFileSystemWatcher watcher;
+
+    bool save;
+
+};
+#endif // MAINWINDOW_H
